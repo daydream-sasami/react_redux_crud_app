cea74209058a19804b58d37a51df03d8b72871d4
daydream-sasami <daydream.sasami@gmail.com>
2019-09-06 23:39:47 +0900

    learn jsx

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">
+          ba
+        </label>
+        <input type="text" onChange={() => {console.log("I am clicked")}}/>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
